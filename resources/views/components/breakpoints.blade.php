90f3ecf9d84cee6c344f2c30217fafd81bcd4cb4
FLAMANT-Lorian <lorian.flamant05@gmail.com>
2026-09-10 10:09:27 +0200

    adds fonts and css preset

---
+++ resources/views/components/breakpoints.blade.php
@@ -0,0 +1,12 @@
+<div class="fixed top-10 left-0 w-fit flex justify-between p-2 z-[999]">
+    <div class="bg-red-500 text-white px-4 py-2 block sm:hidden">XS</div>
+    <div class="bg-blue-500 text-white px-4 py-2 hidden sm:block md:hidden">SM</div>
+    <div class="bg-green-500 text-white px-4 py-2 hidden md:block rg:hidden">MD</div>
+    <div class="bg-yellow-500 text-white px-4 py-2 hidden rg:block rl:hidden">RG</div>
+    <div class="bg-purple-500 text-white px-4 py-2 hidden rl:block lg:hidden">RL</div>
+    <div class="bg-pink-500 text-white px-4 py-2 hidden lg:block xg:hidden">LG</div>
+    <div class="bg-indigo-800 text-white px-4 py-2 hidden xg:block xl:hidden">XG</div>
+    <div class="bg-indigo-500 text-white px-4 py-2 hidden xl:block xxl:hidden">XL</div>
+    <div class="bg-teal-500 text-white px-4 py-2 hidden xxl:block 2xl:hidden">XXL</div>
+    <div class="bg-orange-500 text-white px-4 py-2 hidden 2xl:block">2XL</div>
+</div>
