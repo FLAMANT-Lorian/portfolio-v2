a75bc0aad4948d1f72a4cb525a1581f251687342
FLAMANT-Lorian <lorian.flamant05@gmail.com>
2026-09-10 18:43:17 +0200

    adds pages title

--- resources/views/pages/⚡home/home.php
+++ resources/views/pages/⚡home/home.php
@@ -1,8 +1,9 @@
 <?php
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
-new class extends Component
-{
+new #[Title('partials/header.navigation.home.label')]
+class extends Component {
     //
 };
--- resources/views/pages/⚡projects/projects.php
+++ resources/views/pages/⚡projects/projects.php
@@ -1,8 +1,9 @@
 <?php
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
-new class extends Component
-{
+new #[Title('partials/header.navigation.projects.label')]
+class extends Component {
     //
 };
